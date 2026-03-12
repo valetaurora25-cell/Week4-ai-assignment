84f3a21d9d42d57b8312fbe41368ed0ec62498fd
Rayc Rayc <valetaurora25@gmail.com>
2026-03-12 06:53:20 -0700

    Update assingment.py

--- assingment.py
+++ assingment.py
@@ -1,1 +1,26 @@
+#Question 4 
+print("Hello AI student")  
+
+#Question 5
+name ="John"
+age=20
+city="Nairobi"
+print("Name:",name)
+print("Age:",age) 
+print("city:",city)
+
+#Question 6
+languages=["python","Java","c++","Javascript","Go"]
+print('i',[0])
+
+#Question 7
+students={"name":"John",
+          "age":20,
+          "course":AI student}
+print(students["name"])
+
+#Bonus Question
+#Difference between a list and a dictionary
+#A list stores items in order using indexes languages=["python","java"]
+#A dictionary stores data in key-value pairs 
 
